e83124dfdaededce79fdba93c9e8d00ba3b76087
Minki Baek <alsrl6678@naver.com>
2023-11-23 17:37:03 +0900

    루틴 북마크 버튼 기능 간단 구현

--- loadRoutine.js
+++ loadRoutine.js
@@ -19,14 +19,58 @@ function handleSubmit(event) {
   const $input = document.querySelector("#routine_name");
   const routineName = $input.value;
   // 입력된 값 처리 로직 구현
+  if(routineName.length > 0){
   $Form1.innerHTML += 
-  `<div>
+  `<h3>루틴 : ${routineName}에 대한 운동을 선택합니다.</h3>
+    <div>
         <input type="text id="plus_Workout Placeholder="찾으시는 운동검색"></input>
         <div id="Select">
-        <input type="button" value="북마크✨">
-        </input><input type="button" value="하체"></input><input type="button" value="가슴"></input><input type="button" value="등"></input>
+        <input id="bookmark" type="button" value="북마크✨">
+        </input><input id="leg_ex" type="button" value="하체"></input><input id="chest_ex" type="button" value="가슴"></input><input id="back_ex" type="button" value="등"></input>
     </div>
   `
-    
+  const $Bookmark = document.querySelector("#bookmark");
+  $Bookmark.addEventListener("click" , bookmark)
+
+  const $Leg = document.querySelector("#leg_ex");
+  $Leg.addEventListener("click" ,() => console.log("leg") )
+
+  const $Chest = document.querySelector("#chest_ex");
+  $Chest.addEventListener("click" , () => console.log("chest"))
+
+  const $Back = document.querySelector("#back_ex");
+ $Back.addEventListener("click" , () => console.log("back"))
+
+} else {
+    alert("루틴 제목이 입력 되지않았습니다.")
+}
    
+}
+
+// 어떤 운동 목록 찾고있는지 버튼 
+let button_Switch = 0;
+// bookmark 버튼내용 구현
+function bookmark(){
+    button_Switch = 1;
+    console.log(button_Switch)
+    const $Form1 = document.querySelector("#routineForm");
+    if(button_Switch == 1){
+    $Form1.innerHTML += `
+    <div>
+    <h2>북마크에 저장된 운동이 없습니다.</h2>
+    <span>자주 수행하는 운동들을 북마크에 담아보세요</span>
+    </div>
+    <div>
+    <input type="button" value="운동 추가하기"></input>
+    </div>
+    `
+    button_Switch = 0;
+} else {
+    
+}
+}
+
+// leg 버튼내용 구현
+function leg(){
+    button_Switch = 0;
 }
