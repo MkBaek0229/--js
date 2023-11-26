a245c79df433e85e67b7029901fc1b081415c10f
Minki Baek <alsrl6678@naver.com>
2023-11-26 23:39:45 +0900

    루틴 저장하기 구현 2차

--- loadRoutine.js
+++ loadRoutine.js
@@ -16,8 +16,8 @@ function RoutineBtnclick() {
 
           <div id="save_Routine"> 
               루틴 목록 
-              <ul class="first_ul">
-              </ul>
+              <ol class="first_ul">
+              </ol>
             
           </div>
       `;
@@ -46,11 +46,14 @@ function showRoutineForm() {
   $form.addEventListener("submit", handleSubmit);
 }
 
+let routineName = "";
+
 function handleSubmit(event) {
   const $Form1 = document.querySelector("#routineForm");
   event.preventDefault(); // 기본 제출 동작 방지
   const $input = document.querySelector("#routine_name");
-  const routineName = $input.value;
+  routineName = $input.value;
+  console.log(routineName);
   // 입력된 값 처리 로직 구현
   if (routineName.length > 0) {
     RoutineCount += 1;
@@ -87,40 +90,65 @@ function handleSubmit(event) {
   }
 
   const $saveButton = document.querySelector("#save");
-  $saveButton.addEventListener("click", () => save());
+  $saveButton.addEventListener("click", () => save(routineName));
+}
 
-  function save() {
-    Routine_list.push(routineName);
-    console.log(Routine_list);
-
-    const $Form1 = document.querySelector("#routineForm");
-    $Form1.innerHTML = `<strong>민기님의 루틴</strong><input id="plus_Routine" type="button" value="추가"></input>
+// 저장버튼누르면 처음폼으로돌아가도록
+function save(routineName) {
+  const newWorkObj = {
+    routineName: routineName,
+    id: Date.now(),
+    routineList: selectedExercises,
+  };
+  selectedExercises = [];
+  Routine_list.push(newWorkObj);
+  console.log(Routine_list);
+  const $Form1 = document.querySelector("#routineForm");
+  $Form1.innerHTML = `<strong>민기님의 루틴</strong><input id="plus_Routine" type="button" value="추가"></input>
 
       <p>전체 ${RoutineCount}개</p>
 
-      <div id="save_Routine"> 
+      <div class="first_ul" id="save_Routine"> 
           루틴 목록 
-          <ul class="first_ul">
-          </ul>
+          
         
       </div>
   `;
-    const $first_ul = document.querySelector(".first_ul");
-    $first_ul.innerHTML = Routine_list.map(
-      (Routine) => `<button><h3>${Routine}</h3></button>`
-    ).join("");
-  }
+  const $first_ul = document.querySelector(".first_ul");
+  $first_ul.innerHTML = Routine_list.map(
+    (Routine) =>
+      `<div class="Routine_click" id="${Routine.id}">${Routine.routineName}${Routine.routineList}</div>`
+  ).join("");
 
-  // 어떤 운동 목록 찾고있는지 버튼
-  let button_Switch = 0;
+  const $addButton = document.querySelector("#plus_Routine");
+  $addButton.addEventListener("click", showRoutineForm);
 
-  function bookmark() {
-    button_Switch = 1;
-    console.log(button_Switch);
-    const $choice_work = document.querySelector(".choice_work");
+  const $save_routines = document.querySelectorAll(".Routine_click");
+  console.log($save_routines);
+  $save_routines.forEach(($save_routine) => {
+    $save_routine.addEventListener("click", (event) => {
+      console.log(event);
+    });
+  });
+}
 
-    if (button_Switch == 1) {
-      $choice_work.innerHTML = `
+/*function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
+}*/
+
+// 어떤 운동 목록 찾고있는지 버튼
+let button_Switch = 0;
+
+function bookmark() {
+  button_Switch = 1;
+  console.log(button_Switch);
+  const $choice_work = document.querySelector(".choice_work");
+
+  if (button_Switch == 1) {
+    $choice_work.innerHTML = `
     <div>
       <h2>북마크에 저장된 운동이 없습니다.</h2>
       <span>자주 수행하는 운동들을 북마크에 담아보세요</span>
@@ -129,15 +157,15 @@ function handleSubmit(event) {
     <input id="bookmarkplus_work"type="button" value="운동 추가하기"></input>
     </div>
     `;
-    }
   }
-  // leg 버튼내용 구현
-  function leg() {
-    button_Switch = 2;
-    console.log(button_Switch);
-    const $choice_work = document.querySelector(".choice_work");
-    if (button_Switch == 2) {
-      $choice_work.innerHTML = `
+}
+// leg 버튼내용 구현
+function leg() {
+  button_Switch = 2;
+  console.log(button_Switch);
+  const $choice_work = document.querySelector(".choice_work");
+  if (button_Switch == 2) {
+    $choice_work.innerHTML = `
   <div>
     <button id="squat">스쿼트</button>
     <button id="leg_extension">레그익스텐션</button>
@@ -146,35 +174,36 @@ function handleSubmit(event) {
     <button id="select">뒤로가기</button>
   </div>
   `;
-    }
-    const $squat = document.querySelector("#squat");
-    $squat.addEventListener("click", () => ex_list("스쿼트"));
-
-    const $leg_extension = document.querySelector("#leg_extension");
-    $leg_extension.addEventListener("click", () => ex_list("레그익스텐션"));
-
-    const $lunge = document.querySelector("#lunge");
-    $lunge.addEventListener("click", () => ex_list("런지"));
-
-    const $conventionnal_deadlift = document.querySelector(
-      "#conventionnal_deadlift"
-    );
-    $conventionnal_deadlift.addEventListener("click", () =>
-      ex_list("컨벤셔널 데드리프트")
-    );
-
-    // 뒤로가기 버튼 눌르면 다시 이전화면으로 돌아가게
-    const $select = document.querySelector("#select");
-    $select.addEventListener("click", () => select());
   }
+  const $squat = document.querySelector("#squat");
+  $squat.addEventListener("click", () => ex_list("스쿼트"));
 
-  function select() {
-    const $choice_worklist = document.querySelector(".choice_worklist");
-    $choice_worklist.innerHTML = selectedExercises
-      .map((exercise) => `<li>${exercise}</li>`)
-      .join("");
-    const $choice_work = document.querySelector(".choice_work");
-    $choice_work.innerHTML = `
+  const $leg_extension = document.querySelector("#leg_extension");
+  $leg_extension.addEventListener("click", () => ex_list("레그익스텐션"));
+
+  const $lunge = document.querySelector("#lunge");
+  $lunge.addEventListener("click", () => ex_list("런지"));
+
+  const $conventionnal_deadlift = document.querySelector(
+    "#conventionnal_deadlift"
+  );
+  $conventionnal_deadlift.addEventListener("click", () =>
+    ex_list("컨벤셔널 데드리프트")
+  );
+
+  // 뒤로가기 버튼 눌르면 다시 이전화면으로 돌아가게
+  const $select = document.querySelector("#select");
+  $select.addEventListener("click", () => select());
+}
+
+function select() {
+  const $choice_worklist = document.querySelector(".choice_worklist");
+  $choice_worklist.innerHTML = selectedExercises
+    .map((exercise) => `<li>${exercise}</li>`)
+    .join("");
+  console.log(selectedExercises);
+  const $choice_work = document.querySelector(".choice_work");
+  $choice_work.innerHTML = `
   <input type="text" id="plus_Workout" placeholder="찾으시는 운동 검색"></input>
   <div id="Select">
     <input id="bookmark" type="button" value="북마크✨"></input>
@@ -184,35 +213,36 @@ function handleSubmit(event) {
   </div>
   `;
 
-    const $Bookmark = document.querySelector("#bookmark");
-    $Bookmark.addEventListener("click", bookmark);
+  const $Bookmark = document.querySelector("#bookmark");
+  $Bookmark.addEventListener("click", bookmark);
 
-    const $Leg = document.querySelector("#leg_ex");
-    $Leg.addEventListener("click", leg);
+  const $Leg = document.querySelector("#leg_ex");
+  $Leg.addEventListener("click", leg);
 
-    const $Chest = document.querySelector("#chest_ex");
-    $Chest.addEventListener("click", chest);
+  const $Chest = document.querySelector("#chest_ex");
+  $Chest.addEventListener("click", chest);
 
-    const $Back = document.querySelector("#back_ex");
-    $Back.addEventListener("click", back);
-  }
-  // 선택된 운동 목록 저장 배열
-  let selectedExercises = [];
+  const $Back = document.querySelector("#back_ex");
+  $Back.addEventListener("click", back);
+}
+// 선택된 운동 목록 저장 배열
+let selectedExercises = [];
 
-  function ex_list(choice) {
-    selectedExercises.push(choice);
-    const $choice_worklist = document.querySelector(".choice_worklist");
-    $choice_worklist.innerHTML = selectedExercises
-      .map((exercise) => `<li>${exercise}</li>`)
-      .join("");
-  }
-  // chest 버튼내용 구현
-  function chest() {
-    button_Switch = 3;
-    console.log(button_Switch);
-    const $choice_work = document.querySelector(".choice_work");
-    if (button_Switch == 3) {
-      $choice_work.innerHTML = `
+function ex_list(choice) {
+  selectedExercises.push(choice);
+  console.log(selectedExercises);
+  const $choice_worklist = document.querySelector(".choice_worklist");
+  $choice_worklist.innerHTML = selectedExercises
+    .map((exercise) => `<li>${exercise}</li>`)
+    .join("");
+}
+// chest 버튼내용 구현
+function chest() {
+  button_Switch = 3;
+  console.log(button_Switch);
+  const $choice_work = document.querySelector(".choice_work");
+  if (button_Switch == 3) {
+    $choice_work.innerHTML = `
   <div>
   <button id="bench_press">벤치프레스</button>
   <button id="chest_fly">체스트 플라이</button>
@@ -221,33 +251,33 @@ function handleSubmit(event) {
   <button id="select">뒤로가기</button
   </div>
   `;
-    }
-    const $bench_press = document.querySelector("#bench_press");
-    $bench_press.addEventListener("click", () => ex_list("벤치프레스"));
-
-    const $chest_fly = document.querySelector("#chest_fly");
-    $chest_fly.addEventListener("click", () => ex_list("체스트플라이"));
-
-    const $incine_benchpress = document.querySelector("#incine_benchpress");
-    $incine_benchpress.addEventListener("click", () =>
-      ex_list("인클라인 벤치프레스")
-    );
-
-    const $chest_press = document.querySelector("#chest_press");
-    $chest_press.addEventListener("click", () => ex_list("체스트프레스"));
-
-    // 등록 버튼 눌르면 다시 이전화면으로 돌아가게
-    const $select = document.querySelector("#select");
-    $select.addEventListener("click", () => select());
   }
+  const $bench_press = document.querySelector("#bench_press");
+  $bench_press.addEventListener("click", () => ex_list("벤치프레스"));
 
-  // back 버튼내용 구현
-  function back() {
-    button_Switch = 4;
-    console.log(button_Switch);
-    const $choice_work = document.querySelector(".choice_work");
-    if (button_Switch == 4) {
-      $choice_work.innerHTML = `
+  const $chest_fly = document.querySelector("#chest_fly");
+  $chest_fly.addEventListener("click", () => ex_list("체스트플라이"));
+
+  const $incine_benchpress = document.querySelector("#incine_benchpress");
+  $incine_benchpress.addEventListener("click", () =>
+    ex_list("인클라인 벤치프레스")
+  );
+
+  const $chest_press = document.querySelector("#chest_press");
+  $chest_press.addEventListener("click", () => ex_list("체스트프레스"));
+
+  // 등록 버튼 눌르면 다시 이전화면으로 돌아가게
+  const $select = document.querySelector("#select");
+  $select.addEventListener("click", () => select());
+}
+
+// back 버튼내용 구현
+function back() {
+  button_Switch = 4;
+  console.log(button_Switch);
+  const $choice_work = document.querySelector(".choice_work");
+  if (button_Switch == 4) {
+    $choice_work.innerHTML = `
   <div>
   <button id="barbelrow">바벨로우</button>
   <button id="latfulldown">렛풀다운</button>
@@ -256,21 +286,20 @@ function handleSubmit(event) {
   <button id="select">뒤로가기</button
   </div>
   `;
-    }
-    const $barbelrow = document.querySelector("#barbelrow");
-    $barbelrow.addEventListener("click", () => ex_list("바벨로우"));
-
-    const $latfulldown = document.querySelector("#latfulldown");
-    $latfulldown.addEventListener("click", () => ex_list("렛풀다운"));
-
-    const $seatedrow = document.querySelector("#seatedrow");
-    $seatedrow.addEventListener("click", () => ex_list("시티드로우"));
-
-    const $pullup = document.querySelector("#pullup");
-    $pullup.addEventListener("click", () => ex_list("풀업"));
-
-    // 등록 버튼 눌르면 다시 이전화면으로 돌아가게
-    const $select = document.querySelector("#select");
-    $select.addEventListener("click", () => select());
   }
+  const $barbelrow = document.querySelector("#barbelrow");
+  $barbelrow.addEventListener("click", () => ex_list("바벨로우"));
+
+  const $latfulldown = document.querySelector("#latfulldown");
+  $latfulldown.addEventListener("click", () => ex_list("렛풀다운"));
+
+  const $seatedrow = document.querySelector("#seatedrow");
+  $seatedrow.addEventListener("click", () => ex_list("시티드로우"));
+
+  const $pullup = document.querySelector("#pullup");
+  $pullup.addEventListener("click", () => ex_list("풀업"));
+
+  // 등록 버튼 눌르면 다시 이전화면으로 돌아가게
+  const $select = document.querySelector("#select");
+  $select.addEventListener("click", () => select());
 }
